50baacbc621717704b0d8d8f02f16b269c9e19fa
Neha Gurav <nehgurav@in.ibm.com>
2017-05-12 16:02:08 +0530

    first commit

---
+++ microservice1/src/main/java/com/ibm/MyController.java
@@ -0,0 +1,19 @@
+package com.ibm;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MyController {
+
+	@GetMapping("/hello")
+	public String helloWorld(){
+		
+		
+		
+		return "Hi User";
+	}
+	
+}
